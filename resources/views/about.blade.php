59e2cd9611909fbcdb91a2fc6cf2d6224dfaa0e2
ylledolly <dollesinkarylle@gmail.com>
2024-08-22 14:12:08 +0800

    Added views and routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/about.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Page</h1>
+    <p>Learn more about me.</p>
+</body>
+</html>
---
+++ resources/views/content.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/content.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Content</title>
+</head>
+<body>
+    <h1>Content</h1>
+    <p>This is the content page.</p>
+</body>
+</html>
